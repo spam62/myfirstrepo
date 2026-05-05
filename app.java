c0d5b8d90cae6a8ff0626c466b693073d7a6b81e
spam62 <111337798+spam62@users.noreply.github.com>
2026-05-05 09:41:51 +0530

    Refactor HelloWorld class with multiple versions

--- app.java
+++ app.java
@@ -12,3 +12,8 @@ public class HelloWorld {
         System.out.println("Hello, World!");
     }
 }
+#version 3:
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Prints text to the console
+        System.out.println("Hello, World!");
