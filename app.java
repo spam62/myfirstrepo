9ee0788a030f2b4cf1f4fadf23a0067cc89db04d
spam62 <111337798+spam62@users.noreply.github.com>
2026-05-05 09:47:26 +0530

    Refactor HelloWorld class to remove duplicates
    Removed duplicate class definitions and cleaned up formatting.

--- app.java
+++ app.java
@@ -12,8 +12,3 @@ public class HelloWorld {
         System.out.println("Hello, World!");
     }
 }
-#version 3:
-public class HelloWorld {
-    public static void main(String[] args) {
-        // Prints text to the console
-        System.out.println("Hello, World!");
